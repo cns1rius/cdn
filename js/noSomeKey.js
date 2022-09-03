f71e897ba03fa22a5030d6836236ea7c3cf1af19
cns1rius <cnsirius81@163.com>
2022-09-03 18:58:13 +0800

    '鎻愪氦鏂囦欢'

---
+++ js/noSomeKey.js
@@ -0,0 +1,25 @@
+function forbidKeyboard() {
+    $(document).keydown(function (e) {
+        /*9:Tab键, 17：Control键, 18:Alt键, 123:F12键, 83:S键*/
+        var keyboardCode = [9, 18, 123];
+        for (i in keyboardCode) {
+            if (keyboardCode[i] == e.keyCode) {
+                alert("大黑阔");
+                return false;
+            }
+        }
+        // if ((e.keyCode == 83) && (e.ctrlKey || e.metaKey)) {
+        //     return false;
+        // }
+    });
+    /*禁止文本选择功能*/
+    // $(document).bind("selectstart",function(){return false;});
+}
+$(function () {
+    forbidKeyboard();
+});
+/* 禁止右键 */
+document.oncontextmenu = function () {
+    alert("大黑阔");
+    return false;
+}
